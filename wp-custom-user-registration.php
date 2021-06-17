5b617c8d240b517c9e77a1b6aaf00968ca68c137
Eyu-Tes <eyoabtesfaye10@gmail.com>
2021-06-17 08:59:05 +0300

    Included plugin header

---
+++ wp-custom-user-registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+  Plugin Name: WP Custom User Registration
+  Description: A custom wordpress user registration plugin.
+  Version: 1.0
+  Author: Eyoab Tesfaye
+*/
+
+defined('ABSPATH') || exit;
+
+if (!defined('CR_PLUGIN_FILE')) {
+    define('CR_PLUGIN_FILE', __FILE__);
+}
